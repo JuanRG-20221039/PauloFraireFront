caddbab8b9e2f59695183ded10b8a2fb51f8019f
Marvin SH <20221135@uthh.edu.mx>
2025-10-28 23:23:16 -0600

    cambiando a color amarillo

--- src/user/pages/gamificacion/Libros.jsx
+++ src/user/pages/gamificacion/Libros.jsx
@@ -694,7 +694,7 @@ const Libros = () => {
             className="bg-white rounded-xl shadow-xl p-5 flex flex-col hover:shadow-2xl transition"
           >
             {/* Título */}
-            <h3 className="text-lg font-bold text-blue-700 mb-2 text-center">
+            <h3 className="text-lg font-bold text-yellow-600 mb-2 text-center">
               {libro.nombre}
             </h3>
 
@@ -744,7 +744,7 @@ const Libros = () => {
               )}
 
               <button
-                className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition active:scale-95"
+                className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition active:scale-95"
                 onClick={() => handleLeerMas(libro)}
               >
                 Leer más
